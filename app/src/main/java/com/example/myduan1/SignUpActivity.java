904d7d3c50f034654634b03d32df5cb86204cb9d
manhtai831 <62163420+manhtai831@users.noreply.github.com>
2020-11-17 22:35:36 +0700

    change code layout

--- app/src/main/java/com/example/myduan1/SignUpActivity.java
+++ app/src/main/java/com/example/myduan1/SignUpActivity.java
@@ -1,14 +1,85 @@
 package com.example.myduan1;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.myduan1.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "AAA";
+    private TextInputEditText edSignUpNhapUserName;
+    private TextInputEditText edSignUpNhapLaiPassword;
+    private TextInputEditText edSignUpNhapHoTen;
+    private TextInputEditText edSignUpNhapSDT;
+    private Button btnSignUpDangKy;
+
+
+    private DatabaseReference databaseReference;
+    final String NODE_USER = "user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dang_ky);
+
+        anhXa();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        btnSignUpDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignUp(edSignUpNhapUserName.getText().toString(), new User(edSignUpNhapHoTen.getText().toString(),
+                        edSignUpNhapUserName.getText().toString(),edSignUpNhapLaiPassword.getText().toString(),edSignUpNhapSDT.getText().toString()));
+                Toast.makeText(SignUpActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void SignUp(String s1, User u) {
+        final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this);
+        progressDialog.setMessage("Xin chờ ...");
+        progressDialog.show();
+        databaseReference.child(NODE_USER).child(s1).push().setValue(u);
+        databaseReference.child(NODE_USER).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                progressDialog.dismiss();
+            }
+        });
+    }
+
+    private void anhXa() {
+        edSignUpNhapUserName = (TextInputEditText) findViewById(R.id.ed_SignUp_NhapUserName);
+        edSignUpNhapLaiPassword = (TextInputEditText) findViewById(R.id.ed_SignUp_NhapLaiPassword);
+        edSignUpNhapHoTen = (TextInputEditText) findViewById(R.id.ed_SignUp_NhapHoTen);
+        edSignUpNhapSDT = (TextInputEditText) findViewById(R.id.ed_SignUp_NhapSDT);
+        btnSignUpDangKy = (Button) findViewById(R.id.btn_SignUp_DangKy);
+
     }
 }
---
+++ app/src/main/java/com/example/myduan1/model/User.java
@@ -0,0 +1,47 @@
+package com.example.myduan1.model;
+
+public class User {
+    private String name,userName, password, phoneNumber;
+
+    public User() {
+    }
+
+    public User(String name, String userName, String password, String phoneNumber) {
+        this.name = name;
+        this.userName = userName;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
